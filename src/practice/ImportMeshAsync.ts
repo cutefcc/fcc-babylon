a21b60e8c1eb6c5ba4e4f379beef2d226660497c
cutefcc <cutefcc@126.com>
2022-05-24 23:27:11 +0800

    fix: create box

--- src/practice/ImportMeshAsync.ts
+++ src/practice/ImportMeshAsync.ts
@@ -57,8 +57,9 @@ class GameScene extends BABYLON.Scene {
       "first.glb"
     ).then((result) => {
       console.log("2", result);
-      result.meshes[0].position.x = -1;
-      // result.meshes[0].position.y = 1;
+      result.meshes[0].position.x = -4.5;
+      result.meshes[0].position.y = 0.5;
+      result.meshes[0].position.z = 4.5;
       // const myMesh1 = scene.getMeshByName("myMesh_1");
       // myMesh1.rotation.y = Math.PI / 2;
     });
@@ -66,7 +67,29 @@ class GameScene extends BABYLON.Scene {
 }
 
 createEngine().then((engine) => {
+  // create scene
   const scene = new GameScene(engine);
+  // create ground
+  var ground = BABYLON.MeshBuilder.CreateGround(
+    "ground",
+    { width: 10, height: 10 },
+    scene
+  );
+  let groundMaterial = new BABYLON.StandardMaterial("Ground Material", scene);
+  ground.material = groundMaterial;
+  // create a box
+  const box1 = BABYLON.MeshBuilder.CreateBox("box", {});
+  box1.position.z = -4.5;
+  box1.position.x = -4.5;
+  box1.position.y = 0.5;
+  const box2 = BABYLON.MeshBuilder.CreateBox("box", {});
+  box2.position.z = -4.5;
+  box2.position.x = 4.5;
+  box2.position.y = 0.5;
+  const box3 = BABYLON.MeshBuilder.CreateBox("box", {});
+  box3.position.z = 4.5;
+  box3.position.x = 4.5;
+  box3.position.y = 0.5;
   engine.runRenderLoop(() => {
     scene.render();
   });
