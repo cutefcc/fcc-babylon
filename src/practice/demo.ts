0844ddbad007e4e1e4c8f2d28184fd4f3f965999
cutefcc <cutefcc@126.com>
2022-06-03 22:02:27 +0800

    fix: fountain

--- src/practice/demo.ts
+++ src/practice/demo.ts
@@ -17,6 +17,7 @@ class GameScene extends BABYLON.Scene {
     this.createCamera();
     this.createLight();
     this.#createSkyBox();
+    this.createFountain();
     this.loadModel();
   }
   createCamera() {
@@ -91,6 +92,77 @@ class GameScene extends BABYLON.Scene {
 
     meterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
     skyBox.material = meterial;
+  }
+  // 喷泉⛲️
+  createFountain() {
+    const fountainOutline = [
+      new BABYLON.Vector3(0, 0, 0),
+      new BABYLON.Vector3(0.5, 0, 0),
+      new BABYLON.Vector3(0.5, 0.2, 0),
+      new BABYLON.Vector3(0.4, 0.2, 0),
+      new BABYLON.Vector3(0.4, 0.05, 0),
+      new BABYLON.Vector3(0.05, 0.1, 0),
+      new BABYLON.Vector3(0.05, 0.8, 0),
+      new BABYLON.Vector3(0.15, 0.9, 0),
+    ];
+
+    //Create lathed fountain
+    const fountain = BABYLON.MeshBuilder.CreateLathe("fountain", {
+      shape: fountainOutline,
+      sideOrientation: BABYLON.Mesh.DOUBLESIDE,
+    });
+    fountain.position.x = -4;
+    fountain.position.z = -6;
+
+    const particleSystem = new BABYLON.ParticleSystem("particles", 4000, this);
+    //Texture of each particle
+    particleSystem.particleTexture = new BABYLON.Texture(
+      "http://localhost:8080/images/flare.png",
+      this
+    );
+
+    // Where the particles come from
+    particleSystem.emitter = new BABYLON.Vector3(-4, 1, -6); // the starting object, the emitter
+    particleSystem.minEmitBox = new BABYLON.Vector3(-0.2, 0, 0); // Starting all from
+    particleSystem.maxEmitBox = new BABYLON.Vector3(0.2, 0, 0); // To...
+
+    // Colors of all particles
+    particleSystem.color1 = new BABYLON.Color4(0.7, 0.8, 1.0, 1.0);
+    particleSystem.color2 = new BABYLON.Color4(0.2, 0.5, 1.0, 1.0);
+    particleSystem.colorDead = new BABYLON.Color4(0, 0, 0.2, 0.0);
+
+    // Size of each particle (random between...
+    particleSystem.minSize = 0.01;
+    particleSystem.maxSize = 0.05;
+
+    // Life time of each particle (random between...
+    particleSystem.minLifeTime = 2;
+    particleSystem.maxLifeTime = 3.5;
+
+    // Emission rate
+    particleSystem.emitRate = 1500;
+
+    // Blend mode : BLENDMODE_ONEONE, or BLENDMODE_STANDARD
+    particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+
+    // Set the gravity of all particles
+    particleSystem.gravity = new BABYLON.Vector3(0, -9.81, 0);
+
+    // Direction of each particle after it has been emitted
+    particleSystem.direction1 = new BABYLON.Vector3(-2, 8, 2);
+    particleSystem.direction2 = new BABYLON.Vector3(2, 8, -2);
+
+    // Angular speed, in radians
+    particleSystem.minAngularSpeed = 0;
+    particleSystem.maxAngularSpeed = Math.PI;
+
+    // Speed
+    particleSystem.minEmitPower = 1;
+    particleSystem.maxEmitPower = 3;
+    particleSystem.updateSpeed = 0.025;
+
+    // Start the particle system
+    particleSystem.start();
   }
 }
 
