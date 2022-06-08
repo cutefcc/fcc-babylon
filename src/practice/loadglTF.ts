017baa9e9612899f8044c28d3906d284eabdd0c6
cutefcc <cutefcc@126.com>
2022-06-08 23:33:46 +0800

    fix: load glTF

---
+++ src/practice/loadglTF.ts
@@ -0,0 +1,63 @@
+import * as BABYLON from "@babylonjs/core";
+import "@babylonjs/loaders";
+import "@babylonjs/inspector";
+import gsap from "gsap";
+import * as earcut from "earcut";
+(window as any).earcut = earcut;
+
+const createEngine = async () => {
+  const canvas = document.querySelector<HTMLCanvasElement>("#canvas")!;
+  const engine = new BABYLON.WebGPUEngine(canvas);
+  await engine.initAsync();
+  return engine;
+};
+let switched = false; //on off flag
+class GameScene extends BABYLON.Scene {
+  constructor(engine: BABYLON.Engine) {
+    super(engine);
+    this.createCamera();
+    this.createLight();
+    this.loadModel();
+  }
+  createCamera() {
+    const camera = new BABYLON.ArcRotateCamera(
+      "camera",
+      (Math.PI / 2) * 0.5,
+      Math.PI / 4,
+      16,
+      BABYLON.Vector3.Zero(),
+      this
+    );
+    camera.attachControl(this.getEngine().getRenderingCanvas());
+  }
+
+  createLight() {
+    const light = new BABYLON.HemisphericLight(
+      "light",
+      new BABYLON.Vector3(0, 1, 0),
+      this
+    );
+    light.intensity = 0.7;
+  }
+  async loadModel() {
+    await BABYLON.SceneLoader.ImportMeshAsync(
+      "",
+      "http://localhost:8080/robot_steampunk_3d-coat_4.5_pbr/",
+      "scene.gltf"
+    ).then((result) => {
+      // 缩放
+      result.meshes[0].scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
+    });
+  }
+}
+
+createEngine().then((engine) => {
+  // create scene
+  const scene = new GameScene(engine);
+  scene.debugLayer.show({
+    embedMode: true,
+  });
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
+});
