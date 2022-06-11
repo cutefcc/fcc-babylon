32a13bcb7b94140d75be2dd7d9e2e45b663fe5b4
cutefcc <cutefcc@126.com>
2022-06-12 00:56:47 +0800

    fix: load glTF

--- src/practice/loadglTF.ts
+++ src/practice/loadglTF.ts
@@ -3,6 +3,7 @@ import "@babylonjs/loaders";
 import "@babylonjs/inspector";
 import gsap from "gsap";
 import * as earcut from "earcut";
+import { BabylonFileLoaderConfiguration } from "@babylonjs/core";
 (window as any).earcut = earcut;
 
 const createEngine = async () => {
@@ -47,6 +48,51 @@ class GameScene extends BABYLON.Scene {
     ).then((result) => {
       // 缩放
       result.meshes[0].scaling = new BABYLON.Vector3(0.02, 0.02, 0.02);
+      // 移动
+      result.meshes[0].position.x = 5;
+      result.meshes[0].position.y = 5;
+      result.meshes[0].position.z = 5;
+      // 旋转 这样是不生效的
+      //   result.meshes[0].rotation.y = Math.PI / 2;
+      const sketchfab_model =
+        this.getTransformNodeById("Sketchfab_model")!.getChildren();
+      console.log("sketchfab_model", sketchfab_model);
+      //   gsap.to(result.meshes[0].rotation, {
+      // 这样可以
+      //   gsap.to(result.meshes[0].position, {
+      //     z: 5,
+      //     duration: 4,
+      //   });
+
+      // not working
+      //   gsap.to(result.meshes[0].rotation, {
+      //     y: Math.PI,
+      //     duration: 2,
+      //     repeat: -1,
+      //     ease: "linear",
+      //   });
+      // not working
+      //   gsap.to(sketchfab_model?.rotation, {
+      //     y: Math.PI,
+      //     duration: 2,
+      //     repeat: -1,
+      //     ease: "linear",
+      //   });
+
+      // working
+      result.meshes.forEach((item, index) => {
+        // 这个index === 0 注释后旋转很奇怪
+        if (index === 0) {
+          return;
+        }
+        item.rotation = new BABYLON.Vector3(0, 0, 0);
+        gsap.to(item.rotation, {
+          z: Math.PI * 2,
+          duration: 5,
+          repeat: -1,
+          ease: "linear",
+        });
+      });
     });
   }
 }
